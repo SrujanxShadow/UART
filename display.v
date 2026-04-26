0862a7d7b6a50aa4832c8d948fec7b15939bc59c
Srujan <24ec01052@iitbbs.ac.in>
2026-04-26 10:35:18 +0530

    Update display.v

--- display.v
+++ display.v
@@ -1,1 +1,17 @@
+reg [7:0] led_reg;
+
+    always @(posedge clk or posedge reset)
+begin
+    if (reset)
+        led_reg <= 8'b00000000;
+    else
+    begin
+        if (rx_done == 1)
+            led_reg <= rx_data;
+    end
+end
+
+    assign led = led_reg;
+
+endmodule
 
