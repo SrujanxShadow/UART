2cee876b779bd34de0c5159950d8036c369a3ceb
Srujan <24ec01052@iitbbs.ac.in>
2026-05-14 00:28:02 +0530

    Update top_module.v

--- top_module.v
+++ top_module.v
@@ -1,43 +1,64 @@
-module top_module(
-    input clk,            // 100 MHz clock
-    input reset,          // reset button
-    input rx,             // UART RX from PC
-
-    output tx,            // UART TX to PC
-    output [7:0] led      // LEDs
+module top_module (
+    input clk,
+    input rst,
+    input rx,
+    output tx,
+    output [7:0] led
 );
 
-    wire tick;
-    wire [7:0] rx_data;
-    wire rx_done;
+wire tick, half_tick;
+wire baud_sync;  
+wire [7:0] rx_data;
+wire rx_done;
+wire tx_done;
+wire tx_start;
+wire [7:0] tx_data;
 
-    // Baud Generator
-    baud_generator baud(
-        .clk(clk),
-        .reset(reset),
-        .tick(tick)
-    );
+baud_generator baud (
+    .clk(clk),
+    .rst(rst),
+    .sync_rst(baud_sync),
+    .tick(tick),
+    .half_tick(half_tick)
+);
 
-    // UART Receiver
-    uart_rx rx(
-        .clk(clk),
-        .reset(reset),
-        .tick(tick),
-        .rx(rx),
-        .data_out(rx_data),
-        .done(rx_done)
-    );
+uart_rx rxuut (
+    .clk(clk),
+    .rst(rst),
+    .tick(tick),
+    .half_tick(half_tick),
+    .rx(rx),
+    .data_out(rx_data),
+    .rx_done(rx_done),
+    .baud_sync(baud_sync)
+);
 
-    // UART Transmitter (optional here)
-    uart_tx tx(
-        .clk(clk),
-        .reset(reset),
-        .tick(tick),
-        .start(rx_done),     // echo back received data
-        .data(rx_data),
-        .tx(tx),
-        .done()
-    );
+  echo uut (
+      .clk(clk),
+      .rst(rst),
+      .rx_data(rx_data),
+      .rx_done(rx_done),
+      .tx_done(tx_done),
+      .tx_data(tx_data),
+      .tx_start(tx_start)
+  );
 
-    // LED Display
-    
+uart_tx txuut (
+    .clk(clk),
+    .rst(rst),
+    .tick(tick),
+    .start(tx_start),
+    .data(tx_data),
+    .tx(tx),
+    .tx_done(tx_done)
+);
+
+data dat (
+    .clk(clk),
+    .reset(rst),
+    .rx_data(rx_data),
+    .rx_done(rx_done),
+    .led(led)
+);
+
+endmodule
